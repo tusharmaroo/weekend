3d7f93ae530c0fc2edb31f220bd4496650fb931f
Tushar Maroo <tusharmaroo@gmail.com>
2015-06-18 02:54:51 +0530

    Test cases

--- spec/Weekend_spec.rb
+++ spec/Weekend_spec.rb
@@ -5,7 +5,23 @@ describe Weekend do
     expect(Weekend::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'filter weekends' do
+  	result = Weekend.filter_weekends(Date.today,Date.today+6)
+  	expect(result.count).to eq(5)
+  end
+
+  it 'filter week days' do
+  	result = Weekend.filter_weekdays(Date.today,Date.today+6)
+  	expect(result.count).to eq(2)
+  end
+
+  it 'count no of working days' do
+  	result = Weekend.no_of_working_days(Date.today,Date.today+6)
+  	expect(result).to eq(5)
+  end
+
+  it 'count no of weekends' do
+  	result = Weekend.no_of_weekends(Date.today,Date.today+6)
+  	expect(result).to eq(2)
   end
 end
